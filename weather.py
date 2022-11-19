c185ebe89b5c0db74bed9fd3c421bd20635917bb
Paweł Tumialis <67554101+student180529@users.noreply.github.com>
2022-11-19 15:34:28 +0100

    function

--- weather.py
+++ weather.py
@@ -1,26 +1,25 @@
 from numpy import load
+import scipy.sparse
 
-timestamp = '2022-07-06 00:37:25+00:00'
-print(timestamp)
-timestamp = timestamp[:timestamp.find('+')]
-timestamp = timestamp[:timestamp.rfind(':')]
-hour = int(timestamp[timestamp.rfind(':')-3:timestamp.rfind(':')-1])
-minute = int(timestamp[timestamp.rfind(':')+1:timestamp.rfind(':')+3])
-timestamp = timestamp[:timestamp.rfind(' ')]
-if minute <= 15:
-    minute = 0
-elif minute <= 45:
-    minute = 30
-else:
-    minute = 0
-    hour += 1
+def read_weather(timestamp):
+    #timestamp = '2022-07-06 00:37:25+00:00'
+    #print(timestamp)
+    timestamp = timestamp[:timestamp.find('+')]
+    timestamp = timestamp[:timestamp.rfind(':')]
+    hour = int(timestamp[timestamp.rfind(':')-3:timestamp.rfind(':')-1])
+    minute = int(timestamp[timestamp.rfind(':')+1:timestamp.rfind(':')+3])
+    timestamp = timestamp[:timestamp.rfind(' ')]
+    if minute <= 15:
+        minute = 0
+    elif minute <= 45:
+        minute = 30
+    else:
+        minute = 0
+        hour += 1
 
-file_name ='data/trajectory_challenge/VIL_merc/VIL_merc/VIL-' + timestamp + "-" + "{:02d}".format(hour) + '_' + "{:02d}".format(minute) + 'Z.npz'
+    file_name ='data/trajectory_challenge/VIL_merc/VIL_merc/VIL-' + timestamp + "-" + "{:02d}".format(hour) + '_' + "{:02d}".format(minute) + 'Z.npz'
 
-print(file_name)
+    #print(file_name)
 
-data = load(file_name)
-lst = data.files
-for item in lst:
-    print(item)
-    print(data[item])
+    sparse_matrix = scipy.sparse.load_npz(file_name)
+    return sparse_matrix
